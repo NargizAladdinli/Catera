4258f5dae9aa5988b251112e87849329cee17479
nargizaladdinli <nargizalieva05@gmail.com>
2024-06-30 21:45:44 +0400

    'about'

--- catera/app/about/page.jsx
+++ catera/app/about/page.jsx
@@ -1,4 +1,5 @@
-// 'use client'
+import AboutUs from "../components/AboutUs/AboutUs"
+import GetInTouch from "../components/GetInTouch/GetInTouch"
 import HowItWorks from "../components/HoWItWorks/HowItWorks"
 import { getHowItWorks } from "@/lib/howItWorks"
 
@@ -7,7 +8,9 @@ const About = async({params:lang}) => {
     const howItWorks = await getHowItWorks(lang)
     return(
         <main>
+            <AboutUs/>
             <HowItWorks howItWorks={howItWorks} lang={lang}/>
+            <GetInTouch/>
         </main>
     )
 }
